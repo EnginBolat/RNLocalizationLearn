d2a3d972b1ad76d6a57526fe49283dbf9c0ea60c
Engin Bolat <engi.bolat@gmail.com>
2024-03-17 21:10:43 +0300

    auto detect language property added. translation files path changed

--- src/core/l18n/i18n.config.ts
+++ src/core/l18n/i18n.config.ts
@@ -1,22 +1,27 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import tr from './tr.json';
-import en from './en.json';
+import { en, tr } from './translation';
+import RNLanguageDetector from '@os-team/i18next-react-native-language-detector';
 
 export const languageResource = {
   en: { translation: en },
   tr: { translation: tr },
 };
 
-i18next.use(initReactI18next).init({
-  debug: true,
-  lng: 'en',
-  compatibilityJSON: 'v3',
-  fallbackLng: "en",
-  interpolation: {
-    escapeValue: false,
-  },
-  resources: languageResource,
-})
+i18next
+  .use(RNLanguageDetector)
+  .use(initReactI18next)
+  .init({
+    ns: [],
+    defaultNS: undefined,
+    compatibilityJSON: 'v3',
+    supportedLngs: ['en', 'tr'],
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: languageResource,
+  });
+
 
 export default i18next;
